359511a63b8cd30128d273b2c2bafb5b99577605
Alex <shestmintsev@gmail.com>
2015-04-30 21:54:18 +0300

    full text parcer

--- spec/parser-spec.js
+++ spec/parser-spec.js
@@ -7,24 +7,106 @@ var headers_test = {
   "######header6" : "<h6>header6</h6>",
 }
 
+var paragraf_test = {
+  "It is good test" : "<p>It is good test</p>",
+}
+
+var em_test = {
+  "*test*" : "<p><em>test</em></p>",
+  "*test* this test using *test*" : "<p><em>test</em> this test using <em>test</em></p>",
+}
+
+var strong_test = {
+  "**test**" : "<p><strong>test</strong></p>",
+  "**test** this test using **test**" : "<p><strong>test</strong> this test using <strong>test</strong></p>",
+}
+
+var link_test = {
+  "[example link](http://www.google.com)" : '<p><a href="http://www.google.com">example link</a></p>',
+}
+
+var big_text = [
+  "#PromisePipe - reusable promise chains",
+  "PromisePipe allows to build a reusable Promise chain with custom *API*.",
+  "Promise **pipe** returns a function which you can call multiple times and each time all chains will be called.",
+  "##Link",
+  "[github link](https://github.com/edjafarov)"
+  ];
+
+var big_html = [
+  "<h1>PromisePipe - reusable promise chains</h1>",
+  "<p>PromisePipe allows to build a reusable Promise chain with custom <em>API</em>.</p>",
+  "<p>Promise <strong>pipe</strong> returns a function which you can call multiple times and each time all chains will be called.</p>",
+  "<h2>Link</h2>",
+  '<p><a href="https://github.com/edjafarov">github link</a></p>'
+];
 
 
 
 
-var stringP = "paragraf";
-var stringEm = "*now*";
-var stringEm2 = "*now* is now but is not *now*";
-var stringStrong = "**now**";
-var stringStrong2 = "**now** is now but is not **now**";
 
 
-describe("headers", function () {
-  it("should parser string and return headers (h1,h2,h3) tags", function () {
+describe("Headers", function () {
+  it("should parse string and return headers (h1,h2,h3) tags", function () {
     for (var text in headers_test) {
       var html_check = headers_test[text];
       var html = Parser(text);
-      
+
       expect(html).toBe(html_check);
     }
   });
 });
+
+describe("Paragraf", function () {
+  it("should cover text lines to paragraphs <p>some text</p>", function () {
+    for (var text in paragraf_test) {
+      var html_check = paragraf_test[text];
+      var html = Parser(text);
+
+      expect(html).toBe(html_check);
+    }
+  });
+});
+
+describe("Emphasized", function () {
+  it("should change text wrapped in *...* becomes emphasized <em>text</em>", function () {
+    for (var text in em_test) {
+      var html_check = em_test[text];
+      var html = Parser(text);
+
+      expect(html).toBe(html_check);
+    }
+  });
+});
+
+describe("Strong", function () {
+  it("should change text wrapped in **...** becomes strong <strong>text</strong>", function () {
+    for (var text in strong_test) {
+      var html_check = strong_test[text];
+      var html = Parser(text);
+
+      expect(html).toBe(html_check);
+    }
+  });
+});
+
+describe("Links", function () {
+  it("should parse text like [example link](http://www.google.com) to link <a href='http://www.google.com'>example link</a>", function () {
+    for (var text in link_test) {
+      var html_check = link_test[text];
+      var html = Parser(text);
+
+      expect(html).toBe(html_check);
+    }
+  });
+});
+
+describe("Full text", function () {
+  it("should parse big text", function () {
+    var html_check = big_html.join('');
+    var html = Parser(big_text.join('\n'));
+
+    expect(html).toBe(html_check);
+  });
+});
+
